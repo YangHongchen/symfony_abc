fe39f44b4e1e54df0ce5a8d4a7474ef9875a58a0
Bruce <brucesyang@gmail.com>
2016-07-31 13:08:32 +0800

    渲染一个模板

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -44,4 +44,30 @@ class LuckyController extends Controller
         );
     }
 
+
+    /**
+     * 渲染一个模板：
+     * 这里要做到是通过控制器展现指定的模板的内容
+     * @Route("/lucky/render_a_template")
+     */
+    public function render_a_templateAction()
+    {
+        $number = mt_rand(0, 100);
+
+        // 注意: 模板文件位置不同render方法的第一个参数的写法则不一样:
+
+        //模板在AppBundle/Resource/View/luck目录
+        return $this->render('AppBundle:Lucky:render.html.twig', array(
+            'number' => $number
+        ));
+
+        //模板在 app/view/lucky 目录，可以采用以下写法：
+        //原因在于app/Resouce/views目录相当于是一个模板全局目录，其他bundle都可以直接访问，误区前缀，直接访问该目录下的文件和子目录
+
+        // return $this->render('lucky/render.html.twig', array(
+        //     'number' => $number
+        // ));
+    }
+
+
 }
